8f6844c0455e9bbfb5f18fd854292e4ff882a095
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2025-05-20 09:19:40 -0700

    ignore warning

--- hypothesis-python/tests/common/setup.py
+++ hypothesis-python/tests/common/setup.py
@@ -27,7 +27,13 @@ def run():
     filterwarnings("error")
     filterwarnings("ignore", category=ImportWarning)
     filterwarnings("ignore", category=FutureWarning, module="pandas._version")
-    filterwarnings("ignore", category=DeprecationWarning, module="fakeredis._connection")
+
+    # new warning from Redis 6.0, when calling `FakeRedis()`
+    filterwarnings(
+        "ignore",
+        "Call to '__init__' function with deprecated usage of input argument/s 'retry_on_timeout'",
+        category=DeprecationWarning,
+    )
 
     # See https://github.com/numpy/numpy/pull/432; still a thing as of 2022.
     filterwarnings("ignore", message="numpy.dtype size changed")
